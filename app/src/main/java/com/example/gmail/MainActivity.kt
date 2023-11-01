409aca47ffd79a25de163abccc79f2638e35d6d1
phamHang233 <im.ptt.hang@gmail.com>
2023-11-01 14:50:39 +0700

    first commit

---
+++ app/src/main/java/com/example/gmail/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.gmail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val txtMsg: TextView = findViewById(R.id.txtMsg)
+        val users: Array<String> = arrayOf("HUST", "CTSV", "Le Ba Vui", "Tuto.com", "support")
+        val text_des :Array<String> = arrayOf("Data-0", "Data-1", "Data-2", "Data-3",
+            "Data-4")
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            this,
+            R.layout.item_view,
+            R.id.user_name,
+            users
+            )
+        val listView: ListView = findViewById(R.id.my_list)
+        listView.adapter = adapter
+//        listView.setOnItemClickListener { adapterView, view, i, l -> txtMsg.text =
+//            "Position: $i\nData: ${items[i]}"}
+
+    }
+}
---
+++ app/src/main/java/com/example/gmail/item_view.kt
@@ -0,0 +1,19 @@
+//class NumbersView(// getter method for returning the ID of the imageview
+//    // the resource ID for the imageView
+//    var numbersImageId: Int, // getter method for returning the ID of the TextView 1
+//    // TextView 1
+//    var numberInDigit: String, NumbersInText: String
+//) {
+//
+//    // getter method for returning the ID of the TextView 2
+//    // TextView 1
+//    var numbersInText: String
+//
+//    // create constructor to set the values for all the parameters of the each single view
+//    init {
+//        numbersImageId = numbersImageId;
+//        numberInDigit = numberInDigit;
+//        numbersInText = NumbersInText;
+//    }
+//
+//}
